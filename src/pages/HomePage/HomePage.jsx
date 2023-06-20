a9dd4f254f4bc4ada219b1c598affa7ac694ae39
Joao1V <joaovm.dev@gmail.com>
2023-06-20 17:33:49 -0400

    cards

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,5 @@
 import SEO from "../../Default/SEO.jsx";
+import image from "../../assets/img/img.png"
 
 
 function HomePage() {
@@ -47,13 +48,98 @@ function HomePage() {
                     <h5 className={'text-primary fw-normal mb-8'}>
                         POR QUE ESCOLHER A GENTE
                     </h5>
-                    <h1 className={'fs-2tx mb-14'}>
+                    <h1 className={'fs-2tx mb-14 fw-semibold'}>
                         Compre pelo site, sem complicação
                     </h1>
 
+                    <div className={'container'}>
+                        <div className={'row '}>
+                            <div className={'col-12 col-md-4 col-lg-4'}>
+                                <div className={'card shadow'}>
+                                    <div className={'card-flush'}>
+                                        <div className={''}>
+                                            <img src={image} alt="image" className={'mw-100 rounded-top-2'}/>
+                                        </div>
+                                        <div className={'p-8'}>
+                                            <h3 style={{color:'#0f4229'}}>
+                                                Tudo pelo site
+                                            </h3>
+                                            <div className={'fs-4'} >
+                                                <p>
+                                                    Não precisa entrar em contato com ninguém, todo o processo é feito em um so lugar
+                                                </p>
+                                            </div>
 
-                    <div>
+                                            <div className={'d-flex flex-center pt-4'}>
+                                                <button className={'da-flex gap-2 btn  bg-hover-primary text-primary border border-1 border-primary text-hover-white rounded-pill'}>
+                                                    <span>
+                                                        Saiba mais
+                                                    </span>
+                                                    <i className="ki-solid ki-arrow-right fs-2 text-primary fw-bold p-0"></i>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={'col-12 col-md-4 col-lg-4'}>
+                                <div className={'card shadow'}>
+                                    <div className={'card-flush'}>
+                                        <div className={''}>
+                                            <img src={image} alt="image" className={'mw-100 rounded-top-2'}/>
+                                        </div>
+                                        <div className={'p-8'}>
+                                            <h3 style={{color:'#0f4229'}}>
+                                                Tudo pelo site
+                                            </h3>
+                                            <div className={'fs-4'} >
+                                                <p>
+                                                    Não precisa entrar em contato com ninguém, todo o processo é feito em um so lugar
+                                                </p>
+                                            </div>
 
+                                            <div className={'d-flex flex-center pt-4'}>
+                                                <button className={'da-flex gap-2 btn  bg-hover-primary text-primary border border-1 border-primary text-hover-white rounded-pill'}>
+                                                    <span>
+                                                        Saiba mais
+                                                    </span>
+                                                    <i className="ki-solid ki-arrow-right fs-2 text-primary fw-bold p-0"></i>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={'col-12 col-md-4 col-lg-4'}>
+                                <div className={'card shadow'}>
+                                    <div className={'card-flush'}>
+                                        <div className={''}>
+                                            <img src={image} alt="image" className={'mw-100 rounded-top-2'}/>
+                                        </div>
+                                        <div className={'p-8'}>
+                                            <h3 style={{color:'#0f4229'}}>
+                                                Tudo pelo site
+                                            </h3>
+                                            <div className={'fs-4'} >
+                                                <p>
+                                                    Não precisa entrar em contato com ninguém, todo o processo é feito em um so lugar
+                                                </p>
+                                            </div>
+
+                                            <div className={'d-flex flex-center pt-4'}>
+                                                <button className={'da-flex gap-2 btn  bg-hover-primary text-primary border border-1 border-primary text-hover-white rounded-pill'}>
+                                                    <span>
+                                                        Saiba mais
+                                                    </span>
+                                                    <i className="ki-solid ki-arrow-right fs-2 text-primary fw-bold p-0"></i>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
